926e27c383b64f93b6d17548e29a95e8e59b28c7
Mark Lacerenze <marklacer@gmail.com>
2022-04-05 14:05:27 -0300

    Add CORS

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from database import setup
 from resources.tasks import task_bp
+from flask_cors import CORS 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 setup.create_tables()
+
+@app.before_first_request
+def create_tables():
+  setup.create_tables()
+
 app.register_blueprint(task_bp)
 
 if __name__ == '__main__':
